437964b8a55a9d8fee41e4b88ddf00f506f80cda
Antonio Murgia <antonio.murgia@agilelab.it>
2023-08-30 00:16:03 +0200

    finally

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     id("io.quarkus")
+    id("org.openapi.generator") version "6.6.0"
 }
 
 repositories {
@@ -11,6 +12,7 @@ repositories {
 val quarkusPlatformGroupId: String by project
 val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
+val generatedSourcesDir = "${layout.buildDirectory.get()}/generated/openapi"
 
 dependencies {
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
@@ -19,6 +21,10 @@ dependencies {
     implementation("io.quarkus:quarkus-arc")
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
+
+    implementation("com.github.spotbugs:spotbugs-annotations:4.7.3")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
 }
 
 group = "it.agilelab"
@@ -35,9 +41,30 @@ tasks.withType<Test> {
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
     options.compilerArgs.add("-parameters")
+    dependsOn(tasks.named("openApiGenerate"))
 }
 tasks.quarkusBuild {
     nativeArgs {
         "additional-build-args" to "-H:-CheckToolchain"
+    }
+}
+tasks.openApiGenerate {
+    generatorName.set("java")
+    inputSpec.set("$rootDir/docs/protocol/lake-sharing-protocol-api.yml")
+    library.set("native")
+    outputDir.set(generatedSourcesDir)
+}
+buildscript {
+    configurations.all {
+        resolutionStrategy {
+            force("org.yaml:snakeyaml:1.33")
+        }
+    }
+}
+sourceSets {
+    getByName("main") {
+        java {
+            srcDir("$generatedSourcesDir/src/main/java")
+        }
     }
 }
