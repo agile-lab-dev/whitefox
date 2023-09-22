6c23546e1d66285ec27c221d7503c0133ebd0da4
Marco Scalzo <af39763@enel.com>
2023-09-22 12:39:40 +0200

    run spotlessApply

--- server/src/main/java/io/delta/sharing/api/server/DeltaSharesApiImpl.java
+++ server/src/main/java/io/delta/sharing/api/server/DeltaSharesApiImpl.java
@@ -3,97 +3,101 @@ package io.delta.sharing.api.server;
 import io.delta.sharing.api.server.model.QueryRequest;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import java.math.BigDecimal;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 public class DeltaSharesApiImpl implements SharesApi {
 
-    private final DeltaSharesService deltaSharesService;
+  private final DeltaSharesService deltaSharesService;
 
-    private final ExecutorService executorService;
+  private final ExecutorService executorService;
 
-    @Inject
-    public DeltaSharesApiImpl(DeltaSharesService deltaSharesService,
-                              @ConfigProperty(name= "delta.sharing.api.server.nThreads") int nThreads) {
-        this.deltaSharesService = deltaSharesService;
-        this.executorService = Executors.newFixedThreadPool(nThreads);
-    }
+  @Inject
+  public DeltaSharesApiImpl(
+      DeltaSharesService deltaSharesService,
+      @ConfigProperty(name = "delta.sharing.api.server.nThreads") int nThreads) {
+    this.deltaSharesService = deltaSharesService;
+    this.executorService = Executors.newFixedThreadPool(nThreads);
+  }
 
-    @Override
-    public CompletionStage<Response> getShare(String share) {
-        return CompletableFuture.supplyAsync(() -> deltaSharesService.getShare(share)
+  @Override
+  public CompletionStage<Response> getShare(String share) {
+    return CompletableFuture.supplyAsync(
+        () ->
+            deltaSharesService
+                .getShare(share)
                 .map(s -> Response.ok(s).build())
-                .orElse(Response.status(Response.Status.NOT_FOUND).build()), executorService);
-    }
+                .orElse(Response.status(Response.Status.NOT_FOUND).build()),
+        executorService);
+  }
 
-    @Override
-    public CompletionStage<Response> getTableChanges(
-            String share,
-            String schema,
-            String table,
-            String startingTimestamp,
-            Integer startingVersion,
-            Integer endingVersion,
-            String endingTimestamp,
-            Boolean includeHistoricalMetadata) {
-        Response res = Response.ok().build();
-        return CompletableFuture.completedFuture(res);
-    }
+  @Override
+  public CompletionStage<Response> getTableChanges(
+      String share,
+      String schema,
+      String table,
+      String startingTimestamp,
+      Integer startingVersion,
+      Integer endingVersion,
+      String endingTimestamp,
+      Boolean includeHistoricalMetadata) {
+    Response res = Response.ok().build();
+    return CompletableFuture.completedFuture(res);
+  }
 
-    @Override
-    public CompletionStage<Response> getTableMetadata(
-            String share, String schema, String table, String startingTimestamp) {
-        Response res = Response.ok().build();
-        return CompletableFuture.completedFuture(res);
-    }
+  @Override
+  public CompletionStage<Response> getTableMetadata(
+      String share, String schema, String table, String startingTimestamp) {
+    Response res = Response.ok().build();
+    return CompletableFuture.completedFuture(res);
+  }
 
-    @Override
-    public CompletionStage<Response> getTableVersion(
-            String share, String schema, String table, String startingTimestamp) {
-        Response res = Response.ok().build();
-        return CompletableFuture.completedFuture(res);
-    }
+  @Override
+  public CompletionStage<Response> getTableVersion(
+      String share, String schema, String table, String startingTimestamp) {
+    Response res = Response.ok().build();
+    return CompletableFuture.completedFuture(res);
+  }
 
-    @Override
-    public CompletionStage<Response> listALLTables(
-            String share, BigDecimal maxResults, String pageToken) {
-        Response res = Response.ok().build();
-        return CompletableFuture.completedFuture(res);
-    }
+  @Override
+  public CompletionStage<Response> listALLTables(
+      String share, BigDecimal maxResults, String pageToken) {
+    Response res = Response.ok().build();
+    return CompletableFuture.completedFuture(res);
+  }
 
-    @Override
-    public CompletionStage<Response> listSchemas(
-            String share, BigDecimal maxResults, String pageToken) {
-        Response res = Response.ok().build();
-        return CompletableFuture.completedFuture(res);
-    }
+  @Override
+  public CompletionStage<Response> listSchemas(
+      String share, BigDecimal maxResults, String pageToken) {
+    Response res = Response.ok().build();
+    return CompletableFuture.completedFuture(res);
+  }
 
-    @Override
-    public CompletionStage<Response> listShares(BigDecimal maxResults, String pageToken) {
-        Response res = Response.ok().build();
-        return CompletableFuture.completedFuture(res);
-    }
+  @Override
+  public CompletionStage<Response> listShares(BigDecimal maxResults, String pageToken) {
+    Response res = Response.ok().build();
+    return CompletableFuture.completedFuture(res);
+  }
 
-    @Override
-    public CompletionStage<Response> listTables(
-            String share, String schema, BigDecimal maxResults, String pageToken) {
-        Response res = Response.ok().build();
-        return CompletableFuture.completedFuture(res);
-    }
+  @Override
+  public CompletionStage<Response> listTables(
+      String share, String schema, BigDecimal maxResults, String pageToken) {
+    Response res = Response.ok().build();
+    return CompletableFuture.completedFuture(res);
+  }
 
-    @Override
-    public CompletionStage<Response> queryTable(
-            String share,
-            String schema,
-            String table,
-            QueryRequest queryRequest,
-            String startingTimestamp) {
-        Response res = Response.ok().build();
-        return CompletableFuture.completedFuture(res);
-    }
+  @Override
+  public CompletionStage<Response> queryTable(
+      String share,
+      String schema,
+      String table,
+      QueryRequest queryRequest,
+      String startingTimestamp) {
+    Response res = Response.ok().build();
+    return CompletableFuture.completedFuture(res);
+  }
 }
--- server/src/test/java/io/delta/sharing/api/server/DeltaShareServiceTest.java
+++ server/src/test/java/io/delta/sharing/api/server/DeltaShareServiceTest.java
@@ -1,39 +1,35 @@
 package io.delta.sharing.api.server;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import io.delta.sharing.api.server.model.Share;
 import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.junit.callback.QuarkusTestBeforeClassCallback;
-import io.quarkus.test.junit.callback.QuarkusTestBeforeTestExecutionCallback;
 import io.sharing.persistence.StorageManager;
-import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.util.StringUtils;
-
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
 
 @QuarkusTest
 public class DeltaShareServiceTest {
 
-    @Test
-    public void getUnknownShare() {
-        DeltaSharesService deltaSharesService = new DeltaSharesService(new StorageManager());
-        Optional<Share> unknown = deltaSharesService.getShare("unknown");
-        assertEquals(Optional.empty(), unknown);
-    }
+  @Test
+  public void getUnknownShare() {
+    DeltaSharesService deltaSharesService = new DeltaSharesService(new StorageManager());
+    Optional<Share> unknown = deltaSharesService.getShare("unknown");
+    assertEquals(Optional.empty(), unknown);
+  }
 
-    @Test
-    public void getShare() {
-        ConcurrentMap<String, Share> shares = new ConcurrentHashMap<>();
-        shares.put("key", new Share().id("key").name("name"));
-        DeltaSharesService deltaSharesService = new DeltaSharesService(new StorageManager(shares));
-        Optional<Share> share = deltaSharesService.getShare("key");
-        assertTrue(share.isPresent());
-        assertEquals("name", share.get().getName());
-        assertTrue(StringUtils.isBlank(share.get().getId()));
-    }
-
+  @Test
+  public void getShare() {
+    ConcurrentMap<String, Share> shares = new ConcurrentHashMap<>();
+    shares.put("key", new Share().id("key").name("name"));
+    DeltaSharesService deltaSharesService = new DeltaSharesService(new StorageManager(shares));
+    Optional<Share> share = deltaSharesService.getShare("key");
+    assertTrue(share.isPresent());
+    assertEquals("name", share.get().getName());
+    assertTrue(StringUtils.isBlank(share.get().getId()));
+  }
 }
