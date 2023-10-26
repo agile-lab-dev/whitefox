e9689e71116dcc73dee9d1562111c0bacd902d6c
Antonio Murgia <antonio.murgia@agilelab.it>
2023-10-26 18:42:03 +0200

    bootstrap docsite

---
+++ docsite/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("com.github.node-gradle.node") version("7.0.1")
+}
+
+node {
+    // Whether to download and install a specific Node.js version or not
+    // If false, it will use the globally installed Node.js
+    // If true, it will download node using above parameters
+    // Note that npm is bundled with Node.js
+    download = true
+    // Version of node to download and install (only used if download is true)
+    // It will be unpacked in the workDir
+    version = "20.9.0"
+}
