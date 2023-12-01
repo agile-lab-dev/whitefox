79d23886d1333ab1ddd31e4a9364013451fa25c3
Marco Scalzo <af39763@enel.com>
2023-12-01 11:29:51 +0100

    check also endingVersion

--- server/app/src/main/java/io/whitefox/api/deltasharing/DeltaMappers.java
+++ server/app/src/main/java/io/whitefox/api/deltasharing/DeltaMappers.java
@@ -27,6 +27,8 @@ public class DeltaMappers {
       throw new IllegalArgumentException("The startingVersion and endingVersion are not supported");
     } else if (request.getStartingVersion() != null) {
       throw new IllegalArgumentException("The startingVersion is not supported");
+    } else if (request.getEndingVersion() != null) {
+      throw new IllegalArgumentException("The startingVersion is not supported");
     } else if (request.getVersion() != null && request.getVersion() < 0) {
       throw new IllegalArgumentException("version cannot be negative.");
     } else if (request.getVersion() != null && request.getTimestamp() == null) {
